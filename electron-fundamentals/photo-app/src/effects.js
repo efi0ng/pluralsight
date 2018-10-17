de02e64d9201dba656352c561c31182850768485
efi0ng <git@efiong.net>
2018-10-17 20:39:35 +0100

    moved effects menu logic (including creation) to the effects module.

--- electron-fundamentals/photo-app/src/effects.js
+++ electron-fundamentals/photo-app/src/effects.js
@@ -3,6 +3,10 @@ const EffectNames = {
   ascii: "ascii"
 };
 
+const  EffectMessages = {
+  EFFECT_CHOOSE : 'effect-choose',
+};
+
 const effects = {
   vanilla: (seriously, src, target ) => {
     target.source = src;
@@ -20,11 +24,61 @@ function choose(seriously, src, target, effectName = EffectNames.vanilla) {
   effects[effectName](seriously, src, target);
 }
 
+function cycleToNextEffect(menu) {
+  const allEffects = menu.items.filter(item => item.effect);
+  const selectedIndex = allEffects.findIndex(item => item.checked);
+  allEffects[selectedIndex].checked = false;
+  let nextIndex = selectedIndex + 1;
+  if (nextIndex >= allEffects.length) {
+    nextIndex = 0;
+  }
+  let selectedItem = allEffects[nextIndex];
+  selectedItem.checked = true;
+  return selectedItem.effect;
+}
+
+function makeEffectsMenu(mainWindow) {
+
+  function onEffectItemClick(menuItem) {
+    mainWindow.webContents.send(EffectMessages.EFFECT_CHOOSE, menuItem.effect);
+  }
+
+  function onCycleEffectsClick(menuItem) {
+    let effect = cycleToNextEffect(menuItem.menu);
+    mainWindow.webContents.send(EffectMessages.EFFECT_CHOOSE, effect);
+  }
+
+  return {
+    label: 'Effects',
+    submenu: [
+      {
+        label: 'Cycle',
+        accelerator: 'Shift+CmdOrCtrl+E',
+        click: onCycleEffectsClick
+      },
+      {
+        type: 'separator'
+      },
+      {
+        label: 'Vanilla',
+        effect: EffectNames.vanilla,
+        type: 'radio',
+        click: onEffectItemClick
+      },
+      {
+        label: 'Ascii baby',
+        effect: EffectNames.ascii,
+        type: 'radio',
+        checked: true,
+        click: onEffectItemClick
+      }
+    ]
+  };
+}
+
 module.exports = {
   choose: choose,
   EffectNames: EffectNames,
-  EffectMessages: {
-    EFFECT_CHOOSE : 'effect-choose',
-    EFFECT_CYCLE : 'effect-cycle'
-  }
+  EffectMessages: EffectMessages,
+  makeEffectsMenu: makeEffectsMenu
 };
--- electron-fundamentals/photo-app/src/menu.js
+++ electron-fundamentals/photo-app/src/menu.js
@@ -1,30 +1,8 @@
 const { app } = require('electron');
-const { EffectNames, EffectMessages } = require('./effects');
-
-function cycleToNextEffect(menu) {
-  const allEffects = menu.items.filter(item => item.effect);
-  const selectedIndex = allEffects.findIndex(item => item.checked);
-  allEffects[selectedIndex].checked = false;
-  let nextIndex = selectedIndex + 1;
-  if (nextIndex >= allEffects.length) {
-    nextIndex = 0;
-  }
-  let selectedItem = allEffects[nextIndex];
-  selectedItem.checked = true;
-  return selectedItem.effect;
-}
+const { makeEffectsMenu } = require('./effects');
 
 function makeMenuTemplate(mainWindow) {
   const name = app.getName();
-
-  function onEffectItemClick(menuItem) {
-    mainWindow.webContents.send(EffectMessages.EFFECT_CHOOSE, menuItem.effect);
-  }
-
-  function onCycleEffectsClick(menuItem) {
-    let effect = cycleToNextEffect(menuItem.menu);
-    mainWindow.webContents.send(EffectMessages.EFFECT_CHOOSE, effect);
-  }
 
   const template =
   [{
@@ -54,33 +32,9 @@ function makeMenuTemplate(mainWindow) {
         role: 'quit'
       }
     ]
-  },{
-    label: 'Effects',
-    submenu: [
-      {
-        label: 'Cycle',
-        accelerator: 'Shift+CmdOrCtrl+E',
-        click: onCycleEffectsClick
-      },
-      {
-        type: 'separator'
-      },
-      {
-        label: 'Vanilla',
-        effect: EffectNames.vanilla,
-        type: 'radio',
-        click: onEffectItemClick
-      },
-      {
-        label: 'Ascii baby',
-        effect: EffectNames.ascii,
-        type: 'radio',
-        checked: true,
-        click: onEffectItemClick
-      }
-    ]
-  }
-  ];
+  }];
+
+  template.push(makeEffectsMenu(mainWindow));
 
   return template;
 }
